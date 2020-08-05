d3402d85c78309022869ed0eaee6032a6380505d
jianglulu <2845957073@qq.com>
2020-08-05 09:39:46 +0800

    初始化版本库，实现manage-cli本地脚手架实现自动下载github上面的项目

---
+++ bin/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const program = require("commander"); //命令
+const inquirer = require("inquirer"); //用户交互
+// const { init } = require("../lib/init");
+const clone = require("../lib/init");
+const { exit } = require("process");
+const deleteFolder = require("../lib/deleteFolder");
+const { initInfo, folderExist, rename } = require("../lib/inquirerConfig");
+program
+  .version(require("../package.json").version, "-v --version")
+  .command("init <name>")
+  .action((name) => {
+    if (!fs.existsSync(name)) {
+      inquirer.prompt(initInfo).then((answers) => {
+        clone(name, answers);
+      });
+    } else {
+      inquirer.prompt(folderExist).then(({ recover }) => {
+        if (recover === "cover") {
+          deleteFolder(name);
+          inquirer.prompt(initInfo).then((answers) => {
+            clone(name, answers);
+          });
+        } else if (recover === "new") {
+          inquirer.prompt(rename).then(({ newName }) => {
+            inquirer.prompt(initInfo).then((answers) => {
+              clone(newName, answers);
+            });
+          });
+        } else {
+          exit(1);
+        }
+      });
+    }
+  });
+program.parse(process.argv);
